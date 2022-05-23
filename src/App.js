81fac68de27b49f474c413cabf4b702e02b41e09
Poovendharan Selvaraj <97455003+PoovendharanSelvaraj@users.noreply.github.com>
2022-05-23 14:25:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./components/Counter/Counter";
+import TaskApp from "./components/TaskApp";
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
